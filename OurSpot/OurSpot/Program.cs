0a3818f35fd9a7e7404bd24371943986520e7b7c
carolina <carolina@MONNIE>
2026-09-22 21:08:41 -0400

    creacion y finalizacion  del  ToastService

--- OurSpot/OurSpot/Program.cs
+++ OurSpot/OurSpot/Program.cs
@@ -1,10 +1,16 @@
 using OurSpot.Components;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+
+builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
 
@@ -15,7 +21,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
+
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
