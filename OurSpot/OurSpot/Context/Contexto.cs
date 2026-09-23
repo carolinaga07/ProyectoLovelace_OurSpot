dd14faf698cbc8e4e3d52fbeaaffacb34d2a7a15
carolina <carolina@MONNIE>
2026-09-22 21:16:06 -0400

    creacion y finalizacion del Dbcontext

---
+++ OurSpot/OurSpot/Context/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OurSpot.Context
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
